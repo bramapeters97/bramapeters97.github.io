e2f0bf5c2b42d44d2c84a40c847bb1545ad93547
Bram Peters <130064588+bramapeters97@users.noreply.github.com>
2025-05-22 17:10:43 +0200

    t

---
+++ scripts.js
@@ -0,0 +1,24 @@
+
+<script>
+function includeHTML() {
+    const elements = document.querySelectorAll('[include-html]');
+    elements.forEach(el => {
+        const file = el.getAttribute("include-html");
+        fetch(file)
+            .then(response => {
+                if (!response.ok) throw new Error("Page not found");
+                return response.text();
+            })
+            .then(data => {
+                el.innerHTML = data;
+                el.removeAttribute("include-html");
+                includeHTML(); // handle nested includes
+            })
+            .catch(error => {
+                el.innerHTML = "Error loading file.";
+                console.error(error);
+            });
+    });
+}
+document.addEventListener("DOMContentLoaded", includeHTML);
+</script>
